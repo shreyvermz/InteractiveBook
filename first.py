82c2ea2ad0fbd927a8fce9a62a35a12b33eba02f
Shrey Verma <shreyverma0108@gmail.com>
2024-10-09 12:28:25 -0700

    Added character brainstorms

--- first.py
+++ first.py
@@ -1,9 +1,94 @@
-
+# CARRIAN KREAUX
+    # main character of the book
+    
+    # child of murdered parents Arthur and Vivian
+    
+    # 23 years old, steadfast, promising future
+    
+    # should lead or be heavily involved in investigation process while keeping himself
+    # away from primary suspicion
+    
+    # wants to use the death of his father to spur his family back to their ruthless 
+    # way of life. Hopes that they will rediscover their bloodlust in search of the killer,
+    # or they will forgive the killer and Carrian brings a new age to the family
+     
+    # he wont be suspected much besides his sister, who fails to catch him
+    # either a lack of evidence, convincing the family that he was innocent and they live on
+    # main suspecting character is killed off by him
+        #either by Carrian or a family execution orchestrated by Carrian
+        
+# PENELOPE KREAUX
+    # Carrian's 16 year old sister
+    
+    # handles the parents deaths differently than Carrian
+    
+    # her reaction is seen as more hysterical and more volatile than Carrians,
+    # establishing more family trust with Carrian as he is responsible
+    
+    # dichotomy in reactions causes rest of the family to trust Carrian more
+    # and further villainizing her
+    
+    # she could be the one to accuse Carrian of the murder and bring suspicions of him
+    # to light in front of the whole family, but their perception of her along with
+    # blaming the murder on someone who seems to be the reason they have any leads in
+    # in the first place will lead to her demise
+    
+# ARTHUR KREAUX
+    # father of Carrian and Penelope
+    
+    # kind, distrusting in the killing ways of the Kreauxs
+    
+    # wife, Vivian, died many years ago and he raised Carrian and Penelope by himself
+    
+    # Used to be the most revered hitman in the family and the orchestrator of their grand
+    # plans. For this he was admired by Carrian, who began to idolize the ruthless version
+    # of his father.
+    
+    # His change of ways could be seen as accepted on the exterior by Carrian
+        # Carrian takes a while to come to terms in the shift of Arthur's perspective
+        # adopts a guise of acceptance while beginning his journey to carry on the original
+        # family ways
+    
+    # if Carrians family arrives at the conclusion of forgiving, it should be apparent
+    # that the kindness of Arthur was the reason they gave up their ways
+    
+# MORGAN KREAUX
+    # sister of Arthur
+    # unremarkable, most average member of their family. Thats how it always has been
+    
+    # Didnt resent Arthur, but often felt overshadowed
+    
+    # Carrian may try to convince her to aid him in his plots
+        # after much turmoi and almost joining him, redeems herself and is either
+        # killed by Carrian or is the reason he is caght, if at all
+        
+# MARKO KREAUX
+    # husband of Morgan
+    
+    # not explored too much as a character due to shortness of the story, but is shown
+    # to be somewhat of a father figure to Carrian
+    
+# DORIAN
+    # son of Morgan and Marko
+    
+    # can be the yin to Carrians yang
+        # not sure if he should be a prodigy akin to Carrian or the bottom of the family\'s
+        # barrel in terms of skill set.
+        # He could have the most commendable character of the family, but the Kreauxs dont value
+        # that the same
+        
+    # is the embodiment of kindness and respected Arthur after he changed his ways
+    
+    # finds purpose in urging the Kreauxs to discontinue their murderous ways
+    
+    # can be the main roadblock for Carrian, as they argue over continuing the
+    # life them and their ancestors had known
+    
 def storyboard():
     # gives story if split in this direction
-    print('(first) storyboard in this genre')
+    print('(FIRST) intro story to shift tone more into mystery/thriller')
     # gets choice for after the story
-    userChoice = input('(first) choose 1,2,3')
+    userChoice = input('(FIRST) choose 1,2,3: ')
     
     if userChoice == str(1):
         print('in first choice 1')
@@ -12,4 +97,5 @@ def storyboard():
     elif userChoice == str(3):
         print('in first choice 3')
     else:
-        print('(first) Not a valid input')
+        print('(FIRST) Not a valid input')
+        
